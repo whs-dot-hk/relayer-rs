914a9ef4409ae372bba3362704516fbaa1c6da84
whs <hswongac@gmail.com>
2024-11-04 14:45:19 +0800

    Add boilerplate

--- ibc-rs-contexts/src/lib.rs
+++ ibc-rs-contexts/src/lib.rs
@@ -1,9 +1,88 @@
 use anyhow::Context;
 use prost::Message;
 
+use ibc::clients::tendermint::client_state::ClientState as TmClientState;
+use ibc::clients::tendermint::consensus_state::ConsensusState as TmConsensusState;
+use ibc::clients::tendermint::types::{
+    ClientState as ClientStateType, ConsensusState as ConsensusStateType,
+};
+
+#[derive(ibc::derive::ClientState)]
+#[validation(Ibc)]
+#[execution(Ibc)]
+pub enum AnyClientState {
+    Tendermint(TmClientState),
+}
+
+impl From<ClientStateType> for AnyClientState {
+    fn from(value: ClientStateType) -> Self {
+        Self::Tendermint(value.into())
+    }
+}
+
+impl From<AnyClientState> for ibc::primitives::proto::Any {
+    fn from(value: AnyClientState) -> Self {
+        match value {
+            AnyClientState::Tendermint(v) => v.into(),
+        }
+    }
+}
+
+impl TryFrom<ibc::primitives::proto::Any> for AnyClientState {
+    type Error = ibc::core::host::types::error::DecodingError;
+
+    fn try_from(value: ibc::primitives::proto::Any) -> Result<Self, Self::Error> {
+        if value.type_url == ibc::clients::tendermint::types::TENDERMINT_CLIENT_STATE_TYPE_URL {
+            Ok(Self::Tendermint(value.try_into()?))
+        } else {
+            Err(ibc::core::host::types::error::DecodingError::UnknownTypeUrl(value.type_url))
+        }
+    }
+}
+
+#[derive(ibc::derive::ConsensusState)]
+pub enum AnyConsensusState {
+    Tendermint(TmConsensusState),
+}
+
+impl From<ConsensusStateType> for AnyConsensusState {
+    fn from(value: ConsensusStateType) -> Self {
+        Self::Tendermint(value.into())
+    }
+}
+
+impl From<AnyConsensusState> for ibc::primitives::proto::Any {
+    fn from(value: AnyConsensusState) -> Self {
+        match value {
+            AnyConsensusState::Tendermint(v) => v.into(),
+        }
+    }
+}
+
+impl TryFrom<ibc::primitives::proto::Any> for AnyConsensusState {
+    type Error = ibc::core::host::types::error::DecodingError;
+
+    fn try_from(value: ibc::primitives::proto::Any) -> Result<Self, Self::Error> {
+        if value.type_url == ibc::clients::tendermint::types::TENDERMINT_CONSENSUS_STATE_TYPE_URL {
+            Ok(Self::Tendermint(value.try_into()?))
+        } else {
+            Err(ibc::core::host::types::error::DecodingError::UnknownTypeUrl(value.type_url))
+        }
+    }
+}
+
+impl TryFrom<AnyConsensusState> for ibc::clients::tendermint::types::ConsensusState {
+    type Error = ibc::core::host::types::error::DecodingError;
+    fn try_from(value: AnyConsensusState) -> Result<Self, Self::Error> {
+        match value {
+            AnyConsensusState::Tendermint(c) => Ok(c.inner().clone()),
+        }
+    }
+}
+
 pub struct Ibc {
-    chain_id: ibc::core::host::types::identifiers::ChainId,
-    grpc_addr: String,
+    pub chain_id: ibc::core::host::types::identifiers::ChainId,
+    pub grpc_addr: String,
 }
 
 impl ibc_query::core::context::ProvableContext for Ibc {
@@ -57,7 +136,120 @@ fn get_merkle_proof(
     Ok(ibc_proto::ibc::core::commitment::v1::MerkleProof { proofs })
 }
 
-impl Ibc {
+impl ibc::core::host::ValidationContext for Ibc {
+    type V = Self;
+    type HostClientState = TmClientState;
+    type HostConsensusState = TmConsensusState;
+
+    fn get_client_validation_context(&self) -> &Self::V {
+        unimplemented!()
+    }
+    fn host_timestamp(
+        &self,
+    ) -> Result<ibc::core::primitives::Timestamp, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn host_consensus_state(
+        &self,
+        _height: &ibc::core::client::types::Height,
+    ) -> Result<Self::HostConsensusState, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn client_counter(&self) -> Result<u64, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn connection_end(
+        &self,
+        _conn_id: &ibc::core::host::types::identifiers::ConnectionId,
+    ) -> Result<ibc::core::connection::types::ConnectionEnd, ibc::core::host::types::error::HostError>
+    {
+        unimplemented!()
+    }
+    fn validate_self_client(
+        &self,
+        _client_state_of_host_on_counterparty: Self::HostClientState,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn commitment_prefix(&self) -> ibc::core::commitment_types::commitment::CommitmentPrefix {
+        unimplemented!()
+    }
+    fn connection_counter(&self) -> Result<u64, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn channel_end(
+        &self,
+        _channel_end_path: &ibc::core::host::types::path::ChannelEndPath,
+    ) -> Result<
+        ibc::core::channel::types::channel::ChannelEnd,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn get_next_sequence_send(
+        &self,
+        _seq_send_path: &ibc::core::host::types::path::SeqSendPath,
+    ) -> Result<
+        ibc::core::host::types::identifiers::Sequence,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn get_next_sequence_recv(
+        &self,
+        _seq_recv_path: &ibc::core::host::types::path::SeqRecvPath,
+    ) -> Result<
+        ibc::core::host::types::identifiers::Sequence,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn get_next_sequence_ack(
+        &self,
+        _seq_ack_path: &ibc::core::host::types::path::SeqAckPath,
+    ) -> Result<
+        ibc::core::host::types::identifiers::Sequence,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn get_packet_commitment(
+        &self,
+        _commitment_path: &ibc::core::host::types::path::CommitmentPath,
+    ) -> Result<
+        ibc::core::channel::types::commitment::PacketCommitment,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn get_packet_receipt(
+        &self,
+        _receipt_path: &ibc::core::host::types::path::ReceiptPath,
+    ) -> Result<ibc::core::channel::types::packet::Receipt, ibc::core::host::types::error::HostError>
+    {
+        unimplemented!()
+    }
+    fn get_packet_acknowledgement(
+        &self,
+        _ack_path: &ibc::core::host::types::path::AckPath,
+    ) -> Result<
+        ibc::core::channel::types::commitment::AcknowledgementCommitment,
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+    fn channel_counter(&self) -> Result<u64, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn max_expected_time_per_block(&self) -> std::time::Duration {
+        unimplemented!()
+    }
+    fn validate_message_signer(
+        &self,
+        _signer: &ibc::core::primitives::Signer,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
     fn host_height(
         &self,
     ) -> Result<ibc::core::client::types::Height, ibc::core::host::types::error::HostError> {
@@ -89,4 +281,110 @@ async fn get_latest_block(grpc_addr: &str) -> anyhow::Result<i64> {
         .get_latest_block(GetLatestBlockRequest {})
         .await?;
     get_height(response.get_ref().block.as_ref()).context("no height")
+}
+
+impl ibc::core::client::context::ClientValidationContext for Ibc {
+    type ClientStateRef = AnyClientState;
+    type ConsensusStateRef = AnyConsensusState;
+
+    fn client_state(
+        &self,
+        _client_id: &ibc::core::host::types::identifiers::ClientId,
+    ) -> Result<Self::ClientStateRef, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn consensus_state(
+        &self,
+        _client_cons_state_path: &ibc::core::host::types::path::ClientConsensusStatePath,
+    ) -> Result<Self::ConsensusStateRef, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn client_update_meta(
+        &self,
+        _client_id: &ibc::core::host::types::identifiers::ClientId,
+        _height: &ibc::core::client::types::Height,
+    ) -> Result<
+        (
+            ibc::core::primitives::Timestamp,
+            ibc::core::client::types::Height,
+        ),
+        ibc::core::host::types::error::HostError,
+    > {
+        unimplemented!()
+    }
+}
+
+impl ibc::core::client::context::ClientExecutionContext for Ibc {
+    type ClientStateMut = AnyClientState;
+
+    fn store_client_state(
+        &mut self,
+        _client_state_path: ibc::core::host::types::path::ClientStatePath,
+        _client_state: Self::ClientStateRef,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn store_consensus_state(
+        &mut self,
+        _consensus_state_path: ibc::core::host::types::path::ClientConsensusStatePath,
+        _consensus_state: Self::ConsensusStateRef,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn delete_consensus_state(
+        &mut self,
+        _consensus_state_path: ibc::core::host::types::path::ClientConsensusStatePath,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn store_update_meta(
+        &mut self,
+        _client_id: ibc::core::host::types::identifiers::ClientId,
+        _height: ibc::core::client::types::Height,
+        _host_timestamp: ibc::core::primitives::Timestamp,
+        _host_height: ibc::core::client::types::Height,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn delete_update_meta(
+        &mut self,
+        _client_id: ibc::core::host::types::identifiers::ClientId,
+        _height: ibc::core::client::types::Height,
+    ) -> Result<(), ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+}
+
+impl ibc::core::client::context::ExtClientValidationContext for Ibc {
+    fn host_timestamp(
+        &self,
+    ) -> Result<ibc::core::primitives::Timestamp, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn host_height(
+        &self,
+    ) -> Result<ibc::core::client::types::Height, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn consensus_state_heights(
+        &self,
+        _client_id: &ibc::core::host::types::identifiers::ClientId,
+    ) -> Result<Vec<ibc::core::client::types::Height>, ibc::core::host::types::error::HostError>
+    {
+        unimplemented!()
+    }
+    fn next_consensus_state(
+        &self,
+        _client_id: &ibc::core::host::types::identifiers::ClientId,
+        _height: &ibc::core::client::types::Height,
+    ) -> Result<Option<Self::ConsensusStateRef>, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
+    fn prev_consensus_state(
+        &self,
+        _client_id: &ibc::core::host::types::identifiers::ClientId,
+        _height: &ibc::core::client::types::Height,
+    ) -> Result<Option<Self::ConsensusStateRef>, ibc::core::host::types::error::HostError> {
+        unimplemented!()
+    }
 }
